6a0ed550abc619c253ba99d34b3b4ba7129f353f
Mahmoud-Belghaieb <mahmoud.belghaieb@gmail.com>
2025-04-07 01:54:49 +0100

    Gateway configuration

--- Gateway/src/main/java/com/esprit/microservice/gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/esprit/microservice/gateway/GatewayApplication.java
@@ -2,12 +2,27 @@ package com.esprit.microservice.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("order", r -> r.path("/orders/**")
+						.uri("http://localhost:8081"))
+				.route("product", r -> r.path("/jobs/**")
+						.uri("http://localhost:8082"))
+				.build();
 	}
 
 }
